4db59b2828429639567471fa3a9d55a12167b948
Anujit Ganguly <gangulyanujit@gmail.com>
2020-11-16 22:04:53 +0530

    Add files via upload

---
+++ trial_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int wh, cmp;
+	char comp;
+	cout<<"Enter company name, 52Wh and CMP:"<<endl;
+	cin>>comp;
+	cin>>wh>>cmp;
+	cout<<cmp<<" is the CMP for "<<comp;
+	return 0;
+}
